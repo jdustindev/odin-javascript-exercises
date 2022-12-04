7b7954d05dac45fc87bf2889bdec63ff01b60368
jdustindev <jdustindev@gmail.com>
2022-12-04 15:50:16 -0500

    Complete 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes) {
+  if (numTimes < 0) {
+    return 'ERROR';
+  }
+  let newString = "";
+  for (let i=0; i<numTimes; i++) {
+    newString += string;
+  }
+  return newString;
 };
 
 // Do not edit below this line
