27a4f7ba5bf13de279554656ea93dfc4b3777769
jdustindev <jdustindev@gmail.com>
2022-12-04 16:25:24 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const splitString = string.split('');
+  const reversedString = splitString.reverse();
+  return reversedString.join('');
 };
 
 // Do not edit below this line
