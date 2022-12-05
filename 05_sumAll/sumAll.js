fac3ed33b8bbb16960ff8acc67f81616be451038
jdustindev <jdustindev@gmail.com>
2022-12-04 19:59:52 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    let startNum = a;
+    let endNum = b;
+    if (a > b) {
+        startNum = b;
+        endNum = a;
+    }
+    for(let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
