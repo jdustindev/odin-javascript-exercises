3c3c7ef8e4dfc10b89347682e7af9ced88008c26
jdustindev <jdustindev@gmail.com>
2022-12-06 13:38:59 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const sum = array.reduce(((sum, x) => sum + x), 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((total, x) => total * x, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
