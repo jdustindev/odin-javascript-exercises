dea43d418f974191dca16703b16f42b99616b755
jdustindev <jdustindev@gmail.com>
2022-12-06 15:00:07 -0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+function reverseString(string) {
+    return string.split('').reverse().join('');
+}
 
+const palindromes = function (string) {
+    const trimmedString = string.toLowerCase().replace(/[^0-9a-z]/gi, '');
+    return trimmedString === reverseString(trimmedString);
 };
 
 // Do not edit below this line
