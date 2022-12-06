9a267f7638be7f9d90cbfb760858951827e873da
jdustindev <jdustindev@gmail.com>
2022-12-06 15:16:47 -0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    const indexNumber = Number(index);
+    console.log(indexNumber);
+    if (indexNumber < 1) {
+        return "OOPS";
+    }
+    let a = 0;
+    let b = 1;
+    let current = 1;
+    for (let i = 1; i < indexNumber; i++) {
+        current = a + b;
+        a = b;
+        b = current;
+    }
+    return current;
 };
 
 // Do not edit below this line
