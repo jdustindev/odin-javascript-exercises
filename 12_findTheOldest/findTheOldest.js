c7fc3df68ac3f8681d55d052b1ec0e4a171ae9e0
jdustindev <jdustindev@gmail.com>
2022-12-06 15:28:07 -0500

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = (new Date()).getFullYear();
+        }
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        if (aAge < bAge) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
